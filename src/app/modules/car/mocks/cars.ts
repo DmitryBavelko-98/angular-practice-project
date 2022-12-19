15ff0c8c3f9c4372e2ce939123de7c1ab71471b2
Dmitry Bavelko <bavelko90@gmail.com>
2022-12-19 14:17:26 +0100

    refactor mocks

---
+++ src/app/modules/car/mocks/cars.ts
@@ -0,0 +1,7 @@
+export const CARS = [
+    {id: 1, name: 'BMW M3', color: 'blue', releaseYear: 2022, number: '1234HH', imageUrl: 'assets/img/car/bmw.jpg'},
+    {id: 2, name: 'Mercedes-Benz C', color: 'grey', releaseYear: 2019, number: '7777AA', imageUrl: 'assets/img/car/mercedes.jpg'},
+    {id: 3, name: 'Toyota Camry', color: 'white', releaseYear: 2018, number: '4321BC', imageUrl: 'assets/img/car/toyota.jpg'},
+    {id: 4, name: 'Porsche 911', color: 'red', releaseYear: 2020, number: '1111PC', imageUrl: 'assets/img/car/porsche.jpg'},
+    {id: 5, name: 'Ford Focus', color: 'green', releaseYear: 2018, number: '3124RR', imageUrl: 'assets/img/car/ford.jpg'},
+];
--- src/app/modules/user/mocks/users.ts
+++ src/app/modules/user/mocks/users.ts
@@ -1,7 +1,7 @@
 export const USERS = [
-  {name: 'Ivan', age: 20, isActivated: false},
-  {name: 'Dima', age: 30, isActivated: true},
-  {name: 'Alex', age: 24, isActivated: true},
-  {name: 'Anna', age: 37, isActivated: false},
-  {name: 'Max', age: 16, isActivated: true},
+  {id: 1, name: 'Ivan', age: 20, gender: true, department: 'marketing', company: 'Google', imageUrl: 'assets/img/user/avatar_m.png'},
+  {id: 2, name: 'Dima', age: 22, gender: true, department: 'sales', company: 'Facebook', imageUrl: 'assets/img/user/avatar_m.png'},
+  {id: 3, name: 'Alex', age: 26, gender: true, department: 'accounting', company: 'BMW', imageUrl: 'assets/img/user/avatar_m.png'},
+  {id: 4, name: 'Anna', age: 18, gender: false, department: 'IT', company: 'Google', imageUrl: 'assets/img/user/avatar_f.png'},
+  {id: 5, name: 'Max', age: 33, gender: true, department: 'sales', company: 'Tesla', imageUrl: 'assets/img/user/avatar_m.png'},
 ];
