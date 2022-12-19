a698b156f94a65eff0bae59283d7c52a1629240c
Dmitry Bavelko <bavelko90@gmail.com>
2022-12-19 14:15:30 +0100

    fix car page

--- src/app/modules/car/components/carPage/car-page.component.ts
+++ src/app/modules/car/components/carPage/car-page.component.ts
@@ -1,10 +1,29 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LikeService } from 'src/app/modules/core/services/like.service';
+import ICar from '../../models/car';
+import { CarService } from '../../services/car.service';
 
 @Component({
   selector: 'app-car',
   templateUrl: './car-page.component.html',
   styleUrls: ['./car-page.component.scss'],
 })
-export class CarPageComponent {
+export class CarPageComponent implements OnInit {
   car: string = '';
+  cars: ICar[] = [];
+  likedCars: ICar[] = [];
+
+  constructor(
+    private carService: CarService,
+    private likeService: LikeService
+  ) {}
+
+  ngOnInit(): void {  
+    this.cars = this.carService.getCars();
+    this.likedCars = this.likeService.getCars();
+  }
+
+  checkLikedList(car: any) {
+    this.likeService.addCar(car);
+  }
 }
