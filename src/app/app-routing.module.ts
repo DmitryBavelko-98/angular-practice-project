13551f2dcf7b5eca9918c4943f0753430a281d49
Dmitry Bavelko <bavelko90@gmail.com>
2022-12-22 13:46:41 +0100

    Add header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarPageComponent } from './modules/car/components/carPage/car-page.component';
+import { AddUserPageComponent } from './modules/user/components/add-user-page/add-user-page.component';
 import { UserPageComponent } from './modules/user/components/user-page/user-page.component';
 
 const routes: Routes = [
   {path: '', redirectTo:'/users', pathMatch: 'full'},
   {path: 'users', component: UserPageComponent},
   {path: 'cars', component: CarPageComponent},
+  {path: 'add-user', component: AddUserPageComponent},
 ];
 
 @NgModule({
