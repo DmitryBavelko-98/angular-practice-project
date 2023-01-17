cbb049309d9164733e9f57cb50243717c65990c1
Dmitry Bavelko <bavelko90@gmail.com>
2023-01-17 09:42:26 +0100

    Add progress spinner

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CardComponent } from './components/card/card.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { FavoritesListComponent } from './components/favorites-list/favorites-list.component';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { CardComponent } from './components/card/card.component';
+import { FavoritesListComponent } from './components/favorites-list/favorites-list.component';
 import { GmailValidatorDirective } from './directives/gmail-validator.directive';
 import { ValidationErrorComponent } from './components/validation-error/validation-error.component';
 import { UniqueEmailValidatorDirective } from './directives/unique-email-validator.directive';
 import { AddressFormComponent } from './components/address-form/address-form.component';
 import { AddressesListComponent } from './components/addresses-list/addresses-list.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { GuardPopupComponent } from './components/guard-popup/guard-popup.component';
 import { SearchFieldComponent } from './components/search-field/search-field.component';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
@@ -26,7 +25,6 @@ import { MatDialogModule } from '@angular/material/dialog';
     UniqueEmailValidatorDirective,
     AddressesListComponent,
     AddressFormComponent,
-    GuardPopupComponent,
     SearchFieldComponent
   ],
   imports: [
@@ -35,20 +33,19 @@ import { MatDialogModule } from '@angular/material/dialog';
     MatButtonModule,
     MatIconModule,
     MatInputModule,
-    MatDialogModule,
     ReactiveFormsModule,
+    MatProgressSpinnerModule,
   ],
   exports: [
     CardComponent,
     FavoritesListComponent,
     AddressesListComponent,
     AddressFormComponent,
-    GuardPopupComponent,
     MatCardModule,
     MatButtonModule,
     MatIconModule,
     MatInputModule,
-    MatDialogModule,
+    MatProgressSpinnerModule,
     ValidationErrorComponent,
     ReactiveFormsModule,
     SearchFieldComponent
